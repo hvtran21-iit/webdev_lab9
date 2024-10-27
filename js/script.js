b928e2b893a21980fc7fbda037a8251ca90c0c37
hvtran21-iit <htran21@hawk.iit.edu>
2024-10-27 11:16:12 -0500

    added js code

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
 function init(){
-//add your javascrip between these two lines of code
+//add your javascript between these two lines of code
+  var button = document.getElementById("entrybutton");
  
 
+  function changeText() {
+    var textbox = document.getElementById("entryinput");
+    document.getElementById("textoutput").innerHTML = textbox.value;
+    alert("Henry Tran: " + textbox.value);
+  }
 
 
+  button.addEventListener("click", changeText);
+}
 
-
-
-
-window.addEventListener('load', init);
+window.addEventListener("load", init);
